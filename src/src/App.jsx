a97bcc8d81ab8a7c67f32b14bda4f7651c3a3527
Blinina <anastasia.paancake@gmail.com>
2022-11-03 19:07:39 +0300

    app done

--- src/src/App.jsx
+++ src/src/App.jsx
@@ -1,48 +1,57 @@
 import { React, useState } from "react";
+import axios from 'axios';
+import {
+  url, minPrice, maxPrice, minPayment, maxPayment,
+  minPeriod, maxPeriod
+} from "./utils/utils";
 
 function App() {
+
   const interestRate = 0.035;
   const percent = 100;
-  const url = 'https://hookb.in/eK160jgYJ6UlaRPldJ1P';
   const [cost, setCost] = useState('3300000');
-  const [fee, setFee] = useState('10')
-  const [period, setPeriod] = useState('60')
+  const [fee, setFee] = useState('10');
+  const [period, setPeriod] = useState('60');
+  const [loading, isLoading] = useState(false);
 
   const handleSubmit = async (e) => {
-    const response = await fetch(url, {
-      header: 'application/json', body: {
-        "car_coast": cost,
-        "initail_payment": feeValue,
-        "initail_payment_percent": fee,
-        "lease_term": period,
-        "total_sum": totalSum,
-        "monthly_payment_from": monthPay
-      }
-    });
-    if (!response.ok) {
-      throw new Error('ошибка');
+    e.preventDefault();
+    try {
+      isLoading(true)
+      const data = {
+        car_coast: cost,
+        initail_payment: feeValue,
+        initail_payment_percent: fee,
+        lease_term: period,
+        total_sum: totalSum,
+        monthly_payment_from: monthPay
+      };
+      const response = await axios.post(url, data);
+      console.log(response.status);
+      isLoading(false);
+    } catch (error) {
+      console.log(error)
+      isLoading(false)
     }
-    console.log('все ок')
   }
 
-
   const getCost = (e) => {
-    let {value} = e.target;
-    if (Number(value) > 6000000) {
+    let { value } = e.target;
+    if (value > maxPrice) {
       return;
     }
     setCost(value);
   }
   const getFee = (e) => {
-    let {value} = e.target;
-    if (Number(value) > 60) {
+    let { value } = e.target;
+    if (value > maxPayment) {
       return;
     }
     setFee(value)
   }
   const getPeriod = (e) => {
-    let {value} = e.target;
-    if (Number(value) > 60) {
+    let { value } = e.target;
+    if (value > maxPeriod) {
       return;
     }
     setPeriod(value);
@@ -57,7 +66,7 @@ function App() {
   const feeValue = Math.round(((fee * cost) / percent) * percent) / percent;
   const monthPay = Math.round((cost - feeValue) * ((interestRate * Math.pow((1 + interestRate), period)) / (Math.pow((1 + interestRate), period) - 1)));
   const totalSum = Math.round(feeValue + period * monthPay);
-  
+
   return (
     <div className="app">
       <h1 className='title'>Рассчитайте стоимость автомобиля в лизинг</h1>
@@ -67,10 +76,26 @@ function App() {
             <p>Стоимость автомобиля</p>
             <div className="relative">
               <div className="wrapper-normal">
-                <input type="number" className="inp-normal" value={cost} onChange={(e) => getCost(e)} />
-                <span className="symbols"><h3>₽</h3></span>
+                <input
+                  type="number"
+                  required
+                  className="inp-normal"
+                  value={cost}
+                  onChange={(e) => getCost(e)}
+                  disabled={loading}
+                />
+                <span><h3 className={loading && "span-disable"}>₽</h3></span>
               </div>
-              <input type="range" className="absolute" value={cost} onChange={(e) => getCost(e)} id="cost" name="cost" min="1000000" max="6000000" />
+              <input
+                type="range"
+                className="absolute"
+                value={cost}
+                onChange={(e) => getCost(e)} id="cost"
+                name="cost"
+                min={minPrice}
+                max={maxPrice}
+                disabled={loading}
+              />
             </div>
           </div>
           <div className="item">
@@ -78,24 +103,56 @@ function App() {
             <div className="relative">
               <div className="fee-input field ">
                 <div className="fee-container">
-                  <span><h3>{feeValue}</h3></span>
+                  <span><h3 className={loading && "span-disable"}>{feeValue}</h3></span>
                   <div className="wrapper">
-                    <input type="number" className="fee-field" value={fee} onChange={(e) => getFee(e)} name="fee" />
-                    <span>%</span>
+                    <input
+                      type="number"
+                      className="inp-fee"
+                      value={fee}
+                      onChange={(e) => getFee(e)}
+                      name="fee"
+                      disabled={loading}
+                    />
+                    <span className={loading && "span-disable"}>%</span>
                   </div>
                 </div>
               </div>
-              <input type="range" className="absolute" value={fee} onChange={(e) => getFee(e)} id="fee" name="fee" min="10" max="60" />
+              <input
+                type="range"
+                className="absolute"
+                value={fee}
+                onChange={(e) => getFee(e)}
+                id="fee"
+                name="fee"
+                min={minPayment}
+                max={maxPayment}
+                disabled={loading}
+              />
             </div>
           </div>
           <div className="item">
             <p>Срок лизинга</p>
             <div className="relative">
               <div className="wrapper-normal">
-                <input type="number" className="inp-normal" value={period} onChange={(e) => getPeriod(e)} />
-                <span className="symbols"><h3>мес.</h3></span>
+                <input
+                  type="number"
+                  className="inp-normal"
+                  value={period}
+                  onChange={(e) => getPeriod(e)}
+                  disabled={loading}
+                />
+                <span><h3 className={loading && "span-disable"}>мес.</h3></span>
               </div>
-              <input type="range" className="absolute" value={period} onChange={(e) => getPeriod(e)} id="period" name="period" min="1" max="60" />
+              <input
+                type="range"
+                className="absolute"
+                value={period} onChange={(e) => getPeriod(e)}
+                id="period"
+                name="period"
+                min={minPeriod}
+                max={maxPeriod}
+                disabled={loading}
+              />
             </div>
           </div>
         </div>
@@ -108,7 +165,7 @@ function App() {
             <p className="result-text" >Ежемесячный платеж от</p>
             <h1 className="sum">{monthPay.toLocaleString('ru-RU', localeOptions)}</h1>
           </div>
-          <button type='submit' className="btn"><h3>Оставить заявку</h3></button>
+          <button type='submit' className="btn" disabled={loading}><h3>Оставить заявку</h3></button>
         </div>
       </form>
     </div>
---
+++ src/src/utils/utils.js
@@ -0,0 +1,8 @@
+export const url = 'https://hookb.in/eK160jgYJ6UlaRPldJ1P';
+export const minPrice = 1000000;
+export const maxPrice = 6000000;
+export const minPayment = 10;
+export const maxPayment = 60;
+export const minPeriod = 1;
+export const maxPeriod = 60;
+
